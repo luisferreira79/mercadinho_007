aeb6d360d1f5d1e605cd2823c5a5672e4d848e37
luisferreira79 <86627483+luisferreira79@users.noreply.github.com>
2023-02-06 15:14:51 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+Docker: Utilização prática no cenário de Microsserviços
+Luis Alberto Ferreira - Aluno
+
+Linux - Docker - AWS
---
+++ banco.sql
@@ -0,0 +1,8 @@
+CREATE TABLE dados (
+    CodID int,
+    Produto varchar(50),
+    Secao varchar(50),
+    Corredor varchar(150),
+    Valor int,
+    Host varchar(50)
+);
